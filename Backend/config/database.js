e9566168af9ed74c72d528c30708a85c0f5f83ae
Bereketab Haileeyesus <Bereketab1994@gmail.com>
2024-08-24 13:22:47 +0300

    debugging

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,23 +1,6 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config(); // Load environment variables from .env file
 
-// const db = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
-// db.connect((err) => {
-//   if (err) {
-//     console.error("Error connecting to the database:", err.message);
-//     process.exit(1); // Exit the process if there is a connection error
-//   }
-//   console.log("MySQL Connected...");
-// });
-
-// module.exports = db;
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
