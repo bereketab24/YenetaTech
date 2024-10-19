0e59b2a544ba507f853d524e4f6860b8bd25056d
Bereketab Haileeyesus Shanka <151908471+bereketab24@users.noreply.github.com>
2024-10-19 15:50:38 +0200

    Update auth.routes.js

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const authController = require("../controllers/authController");
 const router = express.Router();
 
+router.get("/check-session", authController.checkSession)
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
