116198e820059c43e96ccc3616a9fa39d42c2552
Bereketab Haileeyesus Shanka <151908471+bereketab24@users.noreply.github.com>
2024-11-20 14:24:06 +0100

    context api for verification

--- Frontend/src/Contexts/Authcontext.jsx
+++ Frontend/src/Contexts/Authcontext.jsx
@@ -1,34 +1,21 @@
-// Import React and the Hooks we need here
-import React, { useState, useEffect, useContext } from "react";
-// Create a context object
-const AuthContext = React.createContext();
-// Create a custom hook to use the context
-export const useAuth = () => {
-  return useContext(AuthContext);
-};
-// Create a provider component
+import React, { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
 export const AuthProvider = ({ children }) => {
-  const [isLogged, setIsLogged] = useState(false);
-  const [isAdmin, setIsAdmin] = useState(false);
-  const [employee, setEmployee] = useState(null);
+  const [authData, setAuthData] = useState({
+    email: "",
+  });
 
-  const value = { isLogged, isAdmin, setIsAdmin, setIsLogged, employee };
+  const updateEmail = (email) => {
+    setAuthData((prev) => ({ ...prev, email }));
+  };
 
-  useEffect(() => {
-    // Retrieve the logged in user from local storage
-    const loggedInEmployee = getAuth();
-    // console.log(loggedInEmployee);
-    loggedInEmployee.then((response) => {
-      // console.log(response);
-      if (response.employee_token) {
-        setIsLogged(true);
-        // 3 is the employee_role for admin
-        if (response.employee_role === 3) {
-          setIsAdmin(true);
-        }
-        setEmployee(response);
-      }
-    });
-  }, []);
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ authData, updateEmail }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
